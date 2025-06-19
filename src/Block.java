fd75296858bf324adebd81b911de82f25f9856c6
Alon <alonso.moros@alumnos.upm.es>
2025-06-19 19:18:49 +0200

    Quitado data y añadido MerkleRoot, añadido metodo para vincular un Bloque con una transacción

--- src/Block.java
+++ src/Block.java
@@ -1,17 +1,20 @@
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; //our data will be a simple message.
-    private long timeStamp; //as number of milliseconds since 1/1/1970.
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<>(); //our data will be a simple message.
+
+    private long timeStamp; // As number of milliseconds since 1/1/1970.
 
     private int nonce;
 
-    //Block Constructor.
-    public Block(String data, String previousHash) {
-        this.data = data;
+    // Block Constructor.
+    public Block(String previousHash) {
+
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash();
@@ -19,19 +22,33 @@ public class Block {
 
     public String calculateHash() {
         return StringUtil.applySha256(
-                previousHash + timeStamp + nonce + data
+                previousHash + timeStamp + nonce + merkleRoot
         );
     }
 
+    //Increases nonce value until hash target is reached.
     public void mineBlock(int difficulty) {
-        String target = new String(new char[difficulty]).replace('\0', '0'); // Create a string with difficulty * "0"
-        while(!hash.substring(0, difficulty).equals(target)) {
-            nonce++;
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
             hash = calculateHash();
-            if (nonce % 1000 == 0) { // Log every 1000 iterations
-                System.out.println("Nonce: " + nonce + " | Hash: " + hash);
-            }
         }
         System.out.println("Block Mined!!! : " + hash);
+    }
+
+    // Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        // Process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 }
